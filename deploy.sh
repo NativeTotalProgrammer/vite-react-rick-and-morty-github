959abcdb1155c32c051714c5b848bdff3e106dcf
Luis Alberto Villar Ortiz <lu.villarortiz@gmail.com>
2023-01-04 17:35:10 -0600

    third commit

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:NativeTotalProgrammer/vite-react-rick-and-morty-github.git main:gh-pages
+git push -f git@github.com:NativeTotalProgrammer/vite-react-rick-and-morty-github.git master:gh-pages
 
 cd -
